902fe5ef45071cc5b452c98b049db88e28fd8d48
sharmababita <sharma.089babita@gmail.com>
2024-11-21 10:42:01 +0100

    Laget med seks elemanter

---
+++ script.js
@@ -0,0 +1,10 @@
+// Green Square
+const squareGreenHide = document.querySelector("#squareGreenHide");
+
+const squareGreenButton= document.querySelector("#squareGreenButton");
+
+console.log(squareGreenHide, squareGreenButton)
+
+squareGreenButton.addEventListener("click", function(){
+  console.log(" Button is clicked!");
+});
