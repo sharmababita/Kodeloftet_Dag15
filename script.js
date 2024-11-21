689d6b4c14cb000763cb92e2b68b1fbd671ef423
sharmababita <sharma.089babita@gmail.com>
2024-11-21 10:49:38 +0100

    Green button is Hide

--- script.js
+++ script.js
@@ -7,4 +7,12 @@ console.log(squareGreenHide, squareGreenButton)
 
 squareGreenButton.addEventListener("click", function(){
   console.log(" Button is clicked!");
+
+  console.log("squareGreenHide");
+
+  console.log("SquaregreenShow");
+
+  squareGreenHide.classList.add("hidden");
 });
+
+''
